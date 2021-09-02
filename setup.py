872812efd7350bc39e2a31a0b2520c7888e70c69
Ilias Agathangelidis <agathangelidis@protonmail.com>
2021-09-02 15:33:46 +0300

    Add links to documentation

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
 
 setup(
-    name='hotspell',
-    version='0.1.0',
-    description='Detect heatwaves',
-    author='Ilias Agathangelidis',
-    packages=['hotspell'],
+    name="hotspell",
+    version="0.1.0",
+    description="Detect heatwaves from weather station data",
+    author="Ilias Agathangelidis",
+    packages=["hotspell"],
+    install_requires=["numpy", "pandas"],
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    include_package_data=True,
+    url="https://github.com/agathangelidis/hotspell",
 )

207633d66a2b229b27522bb75ba3f57737e0fffb
Ilias Agathangelidis <agathangelidis@protonmail.com>
2022-03-03 09:10:55 +0200

    Add data folder to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup
 from pathlib import Path
+
+from setuptools import setup
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.rst").read_text()
@@ -10,6 +11,7 @@ setup(
     description="Detect heat waves from weather station data",
     author="Ilias Agathangelidis",
     packages=["hotspell"],
+    package_data={"hotspell": ["datasets/*.pickle"]},
     install_requires=["numpy", "pandas"],
     long_description=long_description,
     long_description_content_type="text/x-rst",

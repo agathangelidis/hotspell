0d4a6a9d8745ae5df0044b48af7a5a22e82c1333
Ilias Agathangelidis <agathangelidis@protonmail.com>
2022-03-31 14:02:16 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.rst").read_text()
 
 setup(
     name="hotspell",
-    version="0.1.1",
+    version="0.1.2",
     description="Detect heat waves from weather station data",
     author="Ilias Agathangelidis",
     packages=["hotspell"],

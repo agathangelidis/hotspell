3c9bdc4ae40d0cbc8facfef424702f3ab303676a
Ilias Agathangelidis <iliasagath@gmail.com>
2023-11-03 15:06:54 +0200

    Fix readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2023, Ilias Agathangelidis"
 author = "Ilias Agathangelidis"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.5.5"
+release = "0.1.5.6"
 
 
 # -- General configuration ---------------------------------------------------
